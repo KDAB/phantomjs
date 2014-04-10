bf655c3c9b216d19d6f292f8526bf3c66b533e4f
Sergio Martins <iamsergio@gmail.com>
2014-04-10 18:16:34 +0100

    Fix build on SLED 10.4

--- src/breakpad/src/client/linux/handler/exception_handler.cc
+++ src/breakpad/src/client/linux/handler/exception_handler.cc
@@ -97,8 +97,6 @@
 #include "common/linux/eintr_wrapper.h"
 #include "third_party/lss/linux_syscall_support.h"
 
-#include "linux/sched.h"
-
 #ifndef PR_SET_PTRACER
 #define PR_SET_PTRACER 0x59616d61
 #endif
