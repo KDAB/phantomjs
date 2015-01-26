fd8899e3f4b913d85b5e22497fe8c54e97f6c0fe
Milian Wolff <milian.wolff@kdab.com>
2015-01-26 18:13:48 +0100

    Disable breakpad.

--- app/main.cpp
+++ app/main.cpp
@@ -35,22 +35,7 @@
 #include "env.h"
 #include "phantom.h"
 
-#ifdef Q_OS_LINUX
-#include "client/linux/handler/exception_handler.h"
-#endif
-#ifdef Q_OS_MAC
-#include "client/mac/handler/exception_handler.h"
-#endif
-#ifdef Q_OS_WIN32
-#include "client/windows/handler/exception_handler.h"
-#endif
-
 #include <QApplication>
-
-#ifdef Q_OS_WIN32
-using namespace google_breakpad;
-static google_breakpad::ExceptionHandler* eh;
-#endif
 
 #if QT_VERSION != QT_VERSION_CHECK(5, 3, 2)
 #error Something is wrong with the setup. Please report to the mailing list!
@@ -89,33 +74,6 @@ bool exceptionHandler(const char* dump_path, const char* minidump_id, void* cont
 
 int main(int argc, char** argv)
 {
-    // Setup Google Breakpad exception handler
-#ifdef Q_OS_LINUX
-    google_breakpad::ExceptionHandler eh("/tmp", NULL, exceptionHandler, NULL, true);
-#endif
-#ifdef Q_OS_MAC
-    google_breakpad::ExceptionHandler eh("/tmp", NULL, exceptionHandler, NULL, true, NULL);
-#endif
-#ifdef Q_OS_WIN32
-    // This is needed for CRT to not show dialog for invalid param
-    // failures and instead let the code handle it.
-    _CrtSetReportMode(_CRT_ASSERT, 0);
-
-    DWORD cbBuffer = ExpandEnvironmentStrings(TEXT("%TEMP%"), NULL, 0);
-
-    if (cbBuffer == 0) {
-        eh = new ExceptionHandler(TEXT("."), NULL, exceptionHandler, NULL, ExceptionHandler::HANDLER_ALL);
-    } else {
-        LPWSTR szBuffer = reinterpret_cast<LPWSTR>(malloc(sizeof(TCHAR) * (cbBuffer + 1)));
-
-        if (ExpandEnvironmentStrings(TEXT("%TEMP%"), szBuffer, cbBuffer + 1) > 0) {
-            wstring lpDumpPath(szBuffer);
-            eh = new ExceptionHandler(lpDumpPath, NULL, exceptionHandler, NULL, ExceptionHandler::HANDLER_ALL);
-        }
-        free(szBuffer);
-    }
-#endif
-
     QApplication app(argc, argv);
 
     app.setApplicationName("PhantomJS");
